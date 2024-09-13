dfe10e48005fabe03fc67c6dee23be2c17f74a37
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-09-13 19:24:47 +0100

    updates: substring function is manually implemented, unfortunately it is not available in standard library
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

--- stringHandling/main.c
+++ stringHandling/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+
+
 
 /*
  *      strcat(string1, string2)        : string append concatenation  
  *      strlen(string)                  : returning string length
  *      strcpy(str1, str2)              : copying str2(source) to str1(destination) 
  *      strcmp(str1, str2)              : string comparing operation
- *      strrev(str)                     : reverse string
  *      SUBSTRING(str, position, len)   : extracting substring from position to the size
  *      INSERT(mainStr, position, str)  : inserting another string at postion
  *      INDEX(mainStr, subStr)          : return index of the subStr start
@@ -15,6 +18,21 @@
  *
  * */
 
+
+char* substring(char mainstr[], int pos, int sublength) 
+{
+    int i = pos;
+    int j = 0;
+    char* subStr = (char *)malloc(sizeof(char)*(sublength + 1));
+    while(j < sublength)
+    {
+        subStr[j] = mainstr[i];
+        i++;j++;
+    }
+    subStr[j] = '\0';
+
+    return subStr;
+}
 int main()
 {
 
@@ -37,11 +55,17 @@ int main()
     printf("str2: %s\n", str2);
     strcpy(str1, str2);
     printf("str1: %s\n", str1);
-
+    /*string compare operation*/
     if(strcmp(str1, "World") == 0)
         printf("strings are equal\n");
     else
         printf("strings are not equal\n");
+    
+    /*string substrin operation*/
 
+    char mainStr[] = "A quick brown fox jumps over the lazy dog";
+    strcpy(str1, substring(mainStr, 8, 5));
+    printf("str1: %s\n", str1);
+    
     return 0;
 }
