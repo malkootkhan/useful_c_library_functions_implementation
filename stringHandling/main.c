00228ca08c4c281d442c8edf789960b336a3946c
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-09-13 18:33:00 +0100

    updates: two string in C can be merged through standard library function strcat(s1, s2), the interface is defined in "string.h" header file
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

---
+++ stringHandling/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ *      strcat(string1, string2)        : string append concatenation  
+ *      strlen(string)                  : returning string length
+ *      strcmp(str1, str2)              : string comparing operation
+ *      strrev(str)                     : reverse string
+ *      SUBSTRING(str, position, len)   : extracting substring from position to the size
+ *      INSERT(mainStr, position, str)  : inserting another string at postion
+ *      INDEX(mainStr, subStr)          : return index of the subStr start
+ *      DELETE(mainStr, pos, len)       : delete the given length from position
+ *      REPLACE(mainStr,patt1, patt2)   : replace pattern1 in main string with pattern2 
+ *
+ * */
+
+int main()
+{
+
+    /*Implementing strcat(s1, s2)*/
+
+    char str1[] = "Hello ";
+    char str2[] = "World";
+
+    printf("string 1: %s\n", str1);
+    printf("string 2: %s\n", str2);
+    strcat(str1, str2);
+    printf("resulting string: %s\n", str1);
+
+    return 0;
+}
