03f19656040410151626e9b86a6ada089ccf0058
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-09-13 18:55:48 +0100

    updates: Makefile is added for easy compilation process strcmp(s1,s2): return 0 if strings are equal and return value other than 0 if strings are not equal
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

--- stringHandling/main.c
+++ stringHandling/main.c
@@ -38,5 +38,10 @@ int main()
     strcpy(str1, str2);
     printf("str1: %s\n", str1);
 
+    if(strcmp(str1, "World") == 0)
+        printf("strings are equal\n");
+    else
+        printf("strings are not equal\n");
+
     return 0;
 }
